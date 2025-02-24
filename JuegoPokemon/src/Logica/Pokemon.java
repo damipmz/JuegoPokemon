2a100d8c5aea29ce6841691c35c02df44064c25f
damipmz <damianponce97@gmail.com>
2025-02-24 20:32:23 -0300

    aplicando interfaces

--- JuegoPokemon/src/Logica/Pokemon.java
+++ JuegoPokemon/src/Logica/Pokemon.java
@@ -4,5 +4,21 @@ package Logica;
 
 public abstract class Pokemon {
     
+    //PROTECTED Para que solo las clases hijas tengan acceso
+    protected int numPokedex;
+    protected String nombrePokemon;
+    protected double pesoPokemon;
+    protected String sexo;
+    protected int temporadaQueAparece;
+    protected String tipo;
+    
+    
+    //No tiene implementacion
+    protected abstract void atacarPlacaje();
+    
+    protected abstract void atacarAraniazo();
+    
+    protected abstract void atacarMordisco();
+    
     
 }
